73865aa5dab08c41823af83ee008256361c56e67
mwaney <emwaniki91@gmail.com>
2023-06-06 01:23:16 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    word = ""
+    for letter in str:
+        if ord(letter) >= 97 and ord(letter) < 123:
+            word = word + chr(ord(letter) - 32)
+        else:
+            word += letter
+    print(word)
