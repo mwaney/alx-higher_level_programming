20ff23c252b3d4140f0247a1ded23d8c1c0a9c35
mwaney <emwaniki91@gmail.com>
2023-06-06 03:57:52 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ for alpha in range(97, 123):
     if chr(alpha) == "e" or chr(alpha) == "q":
         continue
     else:
-        print(chr(alpha), end = "")
+        print("{}".format(chr(alpha)), end = "")
