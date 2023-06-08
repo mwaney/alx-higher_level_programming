0fb5036cf35a65385580c03a151698995e1c94a5
mwaney <emwaniki91@gmail.com>
2023-06-08 04:39:52 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if (len(argv) == 1):
     print("0 arguments.")
 elif len(argv) > 1:
     if len(argv) == 2:
-        print("1 argument")
+        print("1 argument:")
     else:
-        print("{} arguments".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
     for arg in range(1, len(argv)):
         print("{}: {}".format(arg, argv[arg]))
