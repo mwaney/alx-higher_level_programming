45a9b34ba7ddc626410b50b60c1cf2970d0097af
mwaney <emwaniki91@gmail.com>
2023-06-10 11:33:41 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@
 
 def multiple_returns(sentence):
     if len(sentence) < 1:
-        return (len(sentence), "None" )
+        return (len(sentence), "None")
     else:
         return (len(sentence), sentence[0])
