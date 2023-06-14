a797e81cc34b36e8ee31fea29dbc6da2b09f2ece
mwaney <emwaniki91@gmail.com>
2023-06-14 04:05:50 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+    best = 0
+    best_key = None
+    for key, value in a_dictionary.items():
+        if value > best:
+            best = value
+            best_key = key
+    return best_key
