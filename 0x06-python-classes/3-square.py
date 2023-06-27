79b180b0b9cf3c9cf01835a314cc6c81a28313ef
mwaney <emwaniki91@gmail.com>
2023-06-27 10:21:06 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """A squre class"""
+    """Instantiating the class"""
     def __init__(self, size=0):
         """
         initializing a squre instance.
@@ -18,4 +18,12 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+
+    def area(self):
+        """
+        area is a public instance method
+
+        Returns:
+            int: size multiplied by size
+        """
+        return self.__size * self.__size
