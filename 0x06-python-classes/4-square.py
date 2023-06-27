6935f6a29e9188602a4a979a0d194efab8bffac7
mwaney <emwaniki91@gmail.com>
2023-06-27 10:47:19 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,11 +15,11 @@ class Square:
     @size.setter
     def size(self, value):
         """private instance to set size"""
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """
