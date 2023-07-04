5be245f16313812628b199dfc674ec078fc064bd
mwaney <emwaniki91@gmail.com>
2023-07-04 13:18:31 +0300

    Finish up with the indentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,9 +5,9 @@ Locked class with no class or object attribute,
 
 
 class LockedClass:
-    """
-     prevents the user from dynamically creating
-     new instance attributes,
-     except if the new instance attribute is called first_name
-    """
-    __slots__ = ["first_name"]
+	"""
+	prevents the user from dynamically creating
+	new instance attributes,
+	except if the new instance attribute is called first_name
+	"""
+	__slots__ = ["first_name"]
